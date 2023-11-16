b1dabbf222dc4cfe2bec384acb9510411bd1708d
s32230-pj <146528055+s32230-pj@users.noreply.github.com>
2023-11-16 21:34:51 +0100

    3.1
    Zadanie 3.1

---
+++ 3.1/3.1/3.1.cpp
@@ -0,0 +1,44 @@
+﻿// 3.1.cpp : Ten plik zawiera funkcję „main”. W nim rozpoczyna się i kończy wykonywanie programu.
+//
+
+
+#include <iomanip>
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    float a, b;
+    cout << "Podaj A: ";
+    cin >> a;
+    cout << "Podaj B: ";
+    cin >> b;
+
+    float dodawanie = a + b;
+    float odejmowanie = a - b;
+    float mnozenie = a * b;
+    float dzielenie = a / b;
+
+    cout << setprecision(2) << fixed << "Wynik dodawania:" << dodawanie;
+    cout << endl;
+    cout << setprecision(2) << fixed << "Wynik odejmowania:" << odejmowanie;
+    cout << endl;
+    cout << setprecision(2) << fixed << "Wynik mnozenia:" << mnozenie;
+    cout << endl;
+    cout << setprecision(2) << fixed << "Wynik dzielenia:" << dzielenie;
+    cout << endl;
+
+
+
+}
+
+// Uruchomienie programu: Ctrl + F5 lub menu Debugowanie > Uruchom bez debugowania
+// Debugowanie programu: F5 lub menu Debugowanie > Rozpocznij debugowanie
+
+// Porady dotyczące rozpoczynania pracy:
+//   1. Użyj okna Eksploratora rozwiązań, aby dodać pliki i zarządzać nimi
+//   2. Użyj okna programu Team Explorer, aby nawiązać połączenie z kontrolą źródła
+//   3. Użyj okna Dane wyjściowe, aby sprawdzić dane wyjściowe kompilacji i inne komunikaty
+//   4. Użyj okna Lista błędów, aby zobaczyć błędy
+//   5. Wybierz pozycję Projekt > Dodaj nowy element, aby utworzyć nowe pliki kodu, lub wybierz pozycję Projekt > Dodaj istniejący element, aby dodać istniejące pliku kodu do projektu
+//   6. Aby w przyszłości ponownie otworzyć ten projekt, przejdź do pozycji Plik > Otwórz > Projekt i wybierz plik sln
