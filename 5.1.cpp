c4899ead8e653d71b6c055cc3c7e4e5ebff97091
s32230-pj <146528055+s32230-pj@users.noreply.github.com>
2023-12-15 14:30:50 +0100

    Zadanie 5.1

---
+++ 5.1.cpp
@@ -0,0 +1,44 @@
+﻿
+
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+
+pair<double, int> Max(const vector<double>& wektor) {
+    double maksimum = wektor[0];
+    int id = 0;
+
+    for (int i = 1; i < wektor.size(); ++i) {
+        if (wektor[i] > maksimum) {
+            maksimum = wektor[i];
+            id = i;
+        }
+    }
+
+    return make_pair(maksimum, id);
+}
+
+int main() {
+
+    int liczbaElementow;
+    cout << "Podaj rozmiar wektora: ";
+    cin >> liczbaElementow;
+    vector<double> wektor;
+    for (int i = 0; i < liczbaElementow; ++i) {
+        double element;
+        cout << "Podaj " << i + 1 << " element wektora:";
+        cin >> element;
+        wektor.push_back(element);
+    }
+
+    auto wynik = Max(wektor);
+    double maksimum = wynik.first;
+    int indeksMaksimum = wynik.second;
+    cout << "Najwyzsza wartosc w wektorze: " << maksimum << "ID:" << indeksMaksimum << endl;
+    return 0;
+}
+
+
