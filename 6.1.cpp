8faa6d97d9ebd5b0dc24fe1f290edfe6c76aa8da
s32230-pj <146528055+s32230-pj@users.noreply.github.com>
2024-01-22 14:52:27 +0100

    Zadanie 6.1

---
+++ 6.1.cpp
@@ -0,0 +1,19 @@
+﻿
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout << "Podaj n: ";
+    cin >> n;
+    int liczba = 0;
+    for (int i = 1; i <= n; ++i) {
+        if (i % 5 == 0 && i % 3 != 0) {
+            liczba++;
+        }
+        
+    }
+    cout << "Liczba liczb naturalnych podzielnych przez 5, ale niepodzielnych przez 3 nie wiekszych od: " << n << " wynosi: " << liczba;
+}
+
