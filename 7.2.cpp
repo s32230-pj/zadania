2e09803854472897ee724f955cf8f65484726110
s32230-pj <146528055+s32230-pj@users.noreply.github.com>
2024-01-28 18:15:39 +0100

    Zadanie 7.2

---
+++ 7.2.cpp
@@ -0,0 +1,43 @@
+﻿
+#include <iostream>
+
+using namespace std;
+long long silnia(int n) {
+	if (n < 2)
+		return 1;
+	else
+		return n * silnia(n - 1);
+}
+void podzielnosc(int x) {
+    if (x % 2 == 0) {
+        cout << "2" << endl;
+    }
+    if (x % 3 == 0) {
+        cout << "3" << endl;
+    }
+    if (x % 5 == 0) {
+        cout << "5" << endl;
+    }
+    if (x % 11 == 0) {
+        cout << "11" << endl;
+    }
+    else {
+        cout << " ";
+    }
+}
+
+int main()
+{
+
+	int n;
+	cout << "Podaj liczbe: ";
+	cin >> n;
+	cout << "Silnia liczby: " << n << " wynosi: " << silnia(n) << endl;
+	int x = silnia(n);
+    
+
+	cout << "Liczba jest podzielna przez: " << endl;
+    podzielnosc(x);
+   
+}
+
